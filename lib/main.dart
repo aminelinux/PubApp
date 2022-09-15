60567db5a31013b06bc708eccdf1c1860cda90af
aminelinux <aminehamdi1992@gmail.com>
2022-09-15 12:17:43 +0100

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -32,15 +32,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.r
         primarySwatch: Colors.red,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -50,15 +41,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -70,7 +52,7 @@ class _MyHomePageState extends State<MyHomePage> {
   late Timer _timer;
   final int _counter = 4;
   int _t = 0;
-  List<int> tacheSeconde = [5, 10, 7, 3, 14];
+  List<int> tacheSeconde = [5, 1, 10, 3, 14];
 
   void startTimer() {
     var tempsAlloce = Duration(seconds: tacheSeconde[_t]);
@@ -83,7 +65,7 @@ class _MyHomePageState extends State<MyHomePage> {
       } else {
         setState(() {});
         _t++;
-        if (_t == 4) {
+        if (_t > 4) {
           _t = 0;
         }
       }
@@ -111,7 +93,7 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     const List<String> pub = [
-      "https://www.delice.tn/wp-content/uploads/2021/12/LOGO-MENU.png",
+      "http://localhost/pubserver/images/bison-3840x2160-grand-teton-national-park-wyoming-usa-bing-microsoft-23142.jpg",
       "https://picsum.photos/250?image=9",
       "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c4/Orange-Fruit-Pieces.jpg/220px-Orange-Fruit-Pieces.jpg",
       "https://www.tunisienumerique.com/wp-content/uploads/2019/08/Tunisie-Telecom.png",
@@ -119,29 +101,8 @@ class _MyHomePageState extends State<MyHomePage> {
     ];
 
     return Scaffold(
-      // appBar: AppBar(
-      //   // Here we take the value from the MyHomePage object that was created by
-      //   // the App.build method, and use it to set our appbar title.
-      //   title: Text(widget.title),
-      // ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Image.network(pub[_t]),
@@ -150,7 +111,6 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
